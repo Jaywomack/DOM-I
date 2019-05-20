e1c60fa6ed53e8e368cfe168b8cb180e089a5d96
Jared Womack <womackjw@hotmail.com>
2019-05-20 06:46:37 -0700

    More notes

--- practice.js
+++ practice.js
@@ -87,3 +87,129 @@ newCat.src = 'insert cat image url here' // adds the src to the created element
 newCat.style.marginRight = '20px' //sets a style for the created element
 
 section.prepend(newCat) //adds a child to the beginning of a selected element
+
+/******************************************************************************************8
+ * Selectors Challenge
+ */
+/*
+  Request 1: Log the result of every `a` element on the page. The result should be an `HTMLCollection`
+*/
+anchorTags = document.getElementByTagName('a')
+//const anchorTags;
+console.log(anchorTags);
+
+/* 
+  Request 2: Log the result of every `a` element on the page. The result should be a `NodeList`
+*/
+const moreAnchorTags = document.querySelectorAll('a')
+// const moreAnchorTags;
+ console.log(moreAnchorTags);
+
+/* 
+  Request 3: Locate the ID in the HTML. Log the element that the ID is on.
+*/
+customId = document.getElementById('#first-heading')
+// const customID;
+ console.log(customID);
+
+/* 
+  Request 4: Log the first class that matches ".main-content"
+*/
+cosntmainContent = document.querySelector('.main-content')
+// const mainContent;
+ console.log(mainContent);
+
+/* 
+  Request 5: Log the third element with a class of ".nav-item"
+*/
+const thirsNavItem = document.querySelector('.nav-item')[2]
+// const thirdNavItem;
+console.log(thirdNavItem);
+/******************************************************************************************8
+ * MOST COMMONLY USED METHODS AND PROPERTIES
+ */
+/*
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+.textContent
+Gets and sets the text of an element. Essentially whatever text is between the open and closing tags of an HTML element.
+Can use the assignment operator ( = ) to reset the text of an element
+Setting this property on a node removes all of its children and replaces them with the new single text node.
+<div>Something Here</div>
+element.textContent = 'Something New;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+.setAttribute() (or .{attr})
+This method (or property) is used as a way to set or reassign an attribute on the element.
+‘.setAttribute()’ is a method that takes two arguments, the attribute to set, and the value to set to that attribute.
+eg: element.setAttribute('src', 'http://www.imagsource.com/image.jpg')
+Can also use the pattern: element.'attrName' = ‘value’.
+eg: element.src = 'http://www.imagsource.com/image.jpg'
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+.style
+Every element contains a style object. This property accesses that style object. The style object contains every available style as a key and a value as the value. It is important to note, that these are NOT the CSS styles, these are inline HTML styles.
+These styles are associated with the HTML inline style set on the element
+eg: <div style=“color: red;”>DIV STUFF</div>
+You can access and change a property on the style object by using the assignment operator =.
+eg: element.style.color = ‘blue’;
+Changing a property on the style object will effectively give this element an inline style.
+Inline styles have the highest specificity, overriding any other selector except !important.
+VERY IMPORTANT to note that this does NOT access or change anything in the CSS file.
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+.className, .id
+.className accesses or assigns a string containing all of the classes on the element.
+.id accesses or assigns a string containing the id of the element.
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+.classList
+classList will return an array-like object of all the classes on the element. There are a number of useful methods available on classList.
+classList is a DOMTokenList.
+A DOMTokenList is an array-like object with a numerical zero-based index, a length property, also the .contains() and .forEach() methods.
+Most notably the methods .add() .remove() and .toggle() exist. All three take a single string representing the class.
+.add('className') and .remove('className') do as their names indicate.
+.toggle('className') will add the class if it does not exist and remove it if if does.
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+.appendChild() and .prepend()
+These methods are used for adding child elements to parent elements.
+.appendChild(child) will take an element and add it to it children. It will add it to the ‘end’ physically so if the children are displayed in order it will be the last.
+eg: parentElement.appendChild(childElement)
+.prepend(child) adds a child to the beginning, displaying it first.
+eq: parentElement.prepend(childElement)
+.children and .parentNode
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+These properties are used for accessing relatives of the element.
+.children returns an HTMLCollection of all the children of that element.
+.parentNode returns the parent element of that element.
+
+*/
+/**************************************************************************************
+ * Lecture
+ * Document Object Model with Josh Knell
+ */
+
+ //When we write HTML it gets converted into objects which we call nodes that mostly are elements about 90% the other 10% are txt and other properties
+
+ //single element DOM selectors
+
+ const btnId = document.getElementById()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
